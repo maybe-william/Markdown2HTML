4207161f2fbffbc269b3194cdc61c02ff0d8bcb0
william <191marquis@gmail.com>
2020-05-01 02:02:51 -0400

    [BUGFIX] Use stderr for error messages

--- parser.py
+++ parser.py
@@ -48,13 +48,13 @@ def parseStyles(input):
 
 
 def do_main():
-    from sys import argv
+    from sys import argv, stderr
     from os import path
     if len(argv) < 3:
-        print("Usage: ./markdown2html.py README.md README.html")
+        print("Usage: ./markdown2html.py README.md README.html", file=stderr)
         exit(1)
     if not path.exists(argv[1]):
-        print("Missing {}".format(argv[1]))
+        print("Missing {}".format(argv[1]), file=stderr)
         exit(1)
 
     input = []
