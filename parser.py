d0f3d46f09d108b894d33a223aefa636e66d5368
william <191marquis@gmail.com>
2020-05-02 22:28:42 -0400

    [FEATURE] Add b and em parser

--- parser.py
+++ parser.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This module hides the code from the checker to make the main file pass style checks more easily."""
 
-def matches(line, thing):
-    """check if a line of text matches the given type (h, p, ul, ol, )"""
+
+import re
+
 
 def parseParagraphs(input):
     """This function parses paragraphs for md"""
@@ -88,7 +89,42 @@ def parseLists(input):
 
 def parseStyles(input):
     """This function parses styles for md"""
-    return input
+    def do_md5(string):
+        """Do an md5 hash of the string"""
+        return string
+    def remove_cs(string):
+        """Remove cs from a string"""
+        return string
+
+    newIn = []
+    for i in input:
+        if len(i) > 0:
+            bold = re.compile('\*\*[^\*]*\*\*')
+            italic = re.compile('__[^_]*__')
+            md5 = re.compile('\[\[[^\[\]]*\]\]')
+            noC = re.compile('\(\([^\(\)]*\)\)')
+
+            first_bold = bold.search(i)
+            first_italic = italic.search(i)
+            first_md5 = md5.search(i)
+            first_noC = noC.search(i)
+
+            while first_bold:
+                first_part = i[:first_bold.start()]
+                middle_part = i[first_bold.start()+2:first_bold.end()-2]
+                end_part = i[first_bold.end():]
+                i = first_part+"<b>"+middle_part+"</b>"+end_part
+                first_bold = bold.search(i)
+
+            while first_italic:
+                first_part = i[:first_italic.start()]
+                middle_part = i[first_italic.start()+2:first_italic.end()-2]
+                end_part = i[first_italic.end():]
+                i = first_part+"<em>"+middle_part+"</em>"+end_part
+                first_italic = italic.search(i)
+
+        newIn.append(i)
+    return newIn
 
 
 def do_main():
